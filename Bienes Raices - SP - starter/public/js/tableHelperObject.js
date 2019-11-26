be23b2ca8e6260382427430ebb5bebbc13de281b
Alejandro <alee_2695@live.com.ar>
2019-11-26 12:35:05 -0300

    para parcial

---
+++ Bienes Raices - SP - starter/public/js/tableHelperObject.js
@@ -0,0 +1,46 @@
+
+function crearTabla(array){
+    console.log(array);
+    var tabla=document.createElement("table");
+    tabla.setAttribute('border','1px solid black');
+    tabla.setAttribute('style','border-collapse:collapse');
+    tabla.setAttribute('width','700px');
+
+    tabla.appendChild(creaTh(array[0]));
+    for(var i in array){
+        tabla.appendChild(creaTr(array[i]));
+    }
+
+    return tabla;
+}
+
+function creaTh(object){
+    let cabecera=document.createElement("tr");
+    for(atributo in object.devuelveAtributos()){
+        var th=document.createElement("th");
+        th.textContent=atributo;
+        cabecera.appendChild(th);
+    }
+    return cabecera;
+}
+
+function creaTd(value){
+    let td= document.createElement("td");
+    td.appendChild(document.createTextNode(value));
+    return td;
+}
+
+function creaTr(object){
+    var fila=document.createElement("tr");
+    fila.addEventListener('click',llenaFormulario);
+    fila.appendChild(creaTd(object.id));
+    fila.appendChild(creaTd(object.titulo));
+    fila.appendChild(creaTd(object.transaccion));
+    fila.appendChild(creaTd(object.descripcion));
+    fila.appendChild(creaTd(object.precio));
+    fila.appendChild(creaTd(object.num_wc));
+    fila.appendChild(creaTd(object.num_estacionamiento));
+    fila.appendChild(creaTd(object.num_dormitorio));
+    return fila;
+}
+
